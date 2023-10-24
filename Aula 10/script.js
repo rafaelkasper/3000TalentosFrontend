1ce9e52ffad460a08a7bac1bbc9a8c7d92bfb140
Rafael Kasper <prof.rafaelkasper@gmail.com>
2023-10-24 20:38:15 -0300

    Adicionado código na função de exibir as questões

--- Aula 10/script.js
+++ Aula 10/script.js
@@ -14,20 +14,36 @@ function showQuestion() {
     let q = questions[currentQuestion];
 
     // Criar variável para a barra de progresso baseado na divisão entre número questão atual e quantidade de questões vezes 100. Use a função Math.floor para arredondar.
+    let larguraBarra = Math.floor((currentQuestion / questions.length) * 100);
     // Defina a largura da .progress--bar com o valor obtido
+    document.querySelector(".progress--bar").style.width = `${larguraBarra}%`;
     // Esconda a .scoreArea
+    document.querySelector(".scoreArea").style.display = "none";
     // Exiba a .questionArea
+    document.querySelector(".questionArea").style.display = "block";
     // Insira em .question o valor da questão
+    document.querySelector(".question").innerHTML = q.question;
     // Defina .options como ""
+    document.querySelector(".options").innerHTML = "";
     // Crie uma let optionsHtml para o texto das opções
+    let optionsHtml = "";
     // Faça um laço em q.options e defina o valor da optionHtml com `<div data-op="${i}" class="option"><span>${parseInt(i) + 1}</span>${q.options[i]}</div>`
+
+    for (let i = 0; i < q.options.length; i++) {
+      optionsHtml += `<div data-op="${i}" class="option"><span>${
+        parseInt(i) + 1
+      }</span>${q.options[i]}</div>`;
+    }
+    console.log(optionsHtml);
     // Insira optionsHtml em .options
+    document.querySelector(".options").innerHTML = optionsHtml;
 
     document.querySelectorAll(".options .option").forEach((item) => {
       item.addEventListener("click", optionClickEvent);
     });
   } else {
     // Chame a função finishQuiz
+    finishQuiz();
   }
 }
 
