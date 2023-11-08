080a6264bdd761d8ee57288f49f88dd2b9a56a59
Rafael Kasper <prof.rafaelkasper@gmail.com>
2023-11-08 20:38:59 -0300

    Delete Projeto Blog directory

--- Projeto Blog/README.md
+++
@@ -1,3 +0,0 @@
-# Projeto de blog em React
-
-Revisão de componentização e props
--- Projeto Blog/src/pages/Error.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Error = () => {
-  return <h1>Ops, página não encontrada!</h1>;
-};
-
-export default Error;
