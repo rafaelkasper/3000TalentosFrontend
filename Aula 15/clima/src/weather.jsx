24dcebf1ff02fdc10132977865568db978aa8949
Rafael Kasper <prof.rafaelkasper@gmail.com>
2023-11-10 08:36:55 -0300

    Adicionada estilização

--- Aula 15/clima/src/weather.jsx
+++ Aula 15/clima/src/weather.jsx
@@ -1,93 +1,70 @@
 import axios from "axios";
 import { useState } from "react";
+import "./weather.css";
 
 const Weather = () => {
   const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState({
-    coord: {
-      lon: -51.1472,
-      lat: -29.7603,
-    },
-    weather: [
-      {
-        id: 804,
-        main: "Clouds",
-        description: "overcast clouds",
-        icon: "04n",
-      },
-    ],
-    base: "stations",
-    main: {
-      temp: 20.05,
-      feels_like: 20.23,
-      temp_min: 19.06,
-      temp_max: 20.97,
-      pressure: 1015,
-      humidity: 81,
-    },
-    visibility: 10000,
-    wind: {
-      speed: 2.44,
-      deg: 119,
-      gust: 7.61,
-    },
-    clouds: {
-      all: 100,
-    },
-    dt: 1699572001,
-    sys: {
-      type: 2,
-      id: 2020383,
-      country: "BR",
-      sunrise: 1699518275,
-      sunset: 1699566736,
-    },
-    timezone: -10800,
-    id: 3448622,
-    name: "São Leopoldo",
-    cod: 200,
-  });
+  const [weatherData, setWeatherData] = useState(null);
 
   const apiKey = "";
+  const urlBase = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const imgUrl = "https://openweathermap.org/img/wn/";
 
-  const handleSubmit = async () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt-br`;
-    const response = await axios.get(url);
-    setWeatherData(response.data);
-    console.log(url);
-    setCity("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (city) {
+      try {
+        const url = `${urlBase}${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+        const response = await axios.get(url);
+        setWeatherData(response.data);
+        setCity("");
+      } catch (error) {
+        alert(error);
+      }
+    }
   };
 
   return (
-    <>
-      {!weatherData && (
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Digite a cidade..."
-              onChange={(e) => setCity(e.target.value)}
-            />
-            <button type="submit">Buscar</button>
-          </form>
-        </div>
-      )}
-      {weatherData !== null && (
-        <div>
-          <h6>Tempo agora em </h6>
-          <p>{weatherData.name}</p>
-          <img
-            src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-            width="50"
+    <div className="container">
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Digite a cidade..."
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
           />
-          <p>Temperatura atual: {weatherData.main.temp}º</p>
-          <p>Clima: {weatherData.weather[0].description}</p>
-          <p>Sensação térmica: {weatherData.main.feels_like}</p>
-          <p>Temperatura mínima: {weatherData.main.temp_min}</p>
-          <p>Temperatura máxima{weatherData.main.temp_max}</p>
+          <button type="submit">Buscar</button>
+        </form>
+      </div>
+
+      {weatherData !== null && (
+        <div className="weather-data">
+          <h6>Tempo agora em </h6>
+          <div className="description-container">
+            <h2>
+              {weatherData.name} - {weatherData.sys.country}
+            </h2>
+            <img
+              className="weather-icon"
+              src={`${imgUrl}${weatherData.weather[0].icon}.png`}
+              width="50"
+            />
+            <p className="description">{weatherData.weather[0].description}</p>
+          </div>
+          <div className="details-container">
+            <p className="temperature">{Math.floor(weatherData.main.temp)}ºC</p>
+            <p className="informations">
+              Sensação térmica: {Math.floor(weatherData.main.feels_like)}ºC
+            </p>
+            <p className="informations">
+              Min.: {Math.floor(weatherData.main.temp_min)}ºC Max.:{" "}
+              {Math.floor(weatherData.main.temp_max)}ºC
+            </p>
+          </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
